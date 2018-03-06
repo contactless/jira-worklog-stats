4bc20c6e318c806f773c902e293998d94cf4f6c9
Evgeny Boger <boger@contactless.ru>
2018-03-06 13:38:58 +0300

    add progress bar and optimizations. Loads only worklogs for 120d by default

--- get_worklog.py
+++ get_worklog.py
@@ -1,11 +1,13 @@
-# This script shows how to connect to a JIRA instance with a
+	# This script shows how to connect to a JIRA instance with a
 # username and password over HTTP BASIC authentication.
 
 from jira.client import JIRA
 import sys,csv
+import getpass
+import progressbar
 
-if len(sys.argv) < 4:
-	print "USAGE: script <output.csv> <username> <password>"
+if len(sys.argv) < 3:
+	print "USAGE: script <output.csv> <username>"
 	sys.exit(1)
 
 fname = sys.argv[1]
@@ -17,12 +19,15 @@ fname = sys.argv[1]
 options = {
     'server': 'https://contactless.atlassian.net'
 }
-
-jira = JIRA(options=options, basic_auth=(sys.argv[2], sys.argv[3]))    # a username/password tuple
+password = getpass.getpass()
+jira = JIRA(options=options, basic_auth=(sys.argv[2], password))    # a username/password tuple
 
 # Get the mutable application properties for this server (requires jira-system-administrators permission)
 #~ props = jira.application_properties()
 
+
+pb_widgets = [progressbar.Percentage(), ' ', progressbar.ETA(), progressbar.Bar(left=" [", right="] "), progressbar.SimpleProgress()]
+pb = progressbar.ProgressBar(widgets=pb_widgets, term_width=79).start()
 
 
 with open(fname, "wt") as csvfile:
@@ -31,25 +36,28 @@ with open(fname, "wt") as csvfile:
 	writer.writerow(["issue_id", "worklog_id", "author", "comment", "created", "started", "seconds"])
 
 
-	limit = 1000
+	limit = 100
 	current_pos = 0
 
 	while True:
 		# Find all issues reported by the admin
-		print "get %d issues starting from %d" % (limit, current_pos)
-		issues = jira.search_issues('',startAt=current_pos, maxResults=limit)
+		issues = jira.search_issues('worklogDate>"-120d"',startAt=current_pos, maxResults=limit)
 		#~ issues = jira.search_issues('project=SOFT',maxResults=10000)
-		print len(issues)
 
+		if len(issues) == 0:
+			break
 
-		for issue in issues:
+		pb.maxval = issues.total
+
+		for i, issue in enumerate(issues):
 			for worklog in jira.worklogs(issue):
 				row = [issue.id, worklog.id, worklog.author, worklog.comment, worklog.created, worklog.started, worklog.timeSpentSeconds]
 				row = [unicode(x).encode('utf-8') for x in row]
-				print row
+				# print row
 				writer.writerow(row)
 
-		if len(issues) >= limit:
-			current_pos += len(issues)
-		else:
-			break
+			pb.update(current_pos + i)
+
+		current_pos += len(issues)
+
+pb.finish()
